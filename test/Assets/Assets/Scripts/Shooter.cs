a0cade3e5661a4717e24c09c99697dfbc744d9e1
assafgames <assaf.ipad42@gmail.com>
2018-04-08 00:25:50 +0300

    Butify Lion area

---
+++ test/Assets/Assets/Scripts/Shooter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+
+    public Rigidbody projectile;
+    public Transform gun;
+
+    int bulletSpeed = 20;
+
+    void Update()
+    {
+        // Put this in your update function
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            // Instantiate the projectile at the position and rotation of this transform
+            Rigidbody clone = Instantiate(projectile, gun.transform.position, gun.transform.rotation);
+
+            clone.AddForce(gun.transform.forward*bulletSpeed);
+
+            Destroy(clone.gameObject, 3);
+        }
+    }
+}
