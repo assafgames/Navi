09e627cbced41bcde76c0718969181a4054da0c6
assafgames <assaf.ipad42@gmail.com>
2018-04-15 00:46:53 +0300

    zabra fiels

--- test/Assets/Assets/Scripts/Shooter.cs
+++ test/Assets/Assets/Scripts/Shooter.cs
@@ -7,21 +7,19 @@ public class Shooter : MonoBehaviour
 
     public Rigidbody projectile;
     public Transform gun;
-
-    int bulletSpeed = 20;
-
+    public int bulletSpeed = 20;
+    public int bulletLifeTime = 3;
     void Update()
     {
         // Put this in your update function
         if (Input.GetMouseButtonDown(0))
         {
-
             // Instantiate the projectile at the position and rotation of this transform
             Rigidbody clone = Instantiate(projectile, gun.transform.position, gun.transform.rotation);
 
-            clone.AddForce(gun.transform.forward*bulletSpeed);
+            clone.velocity = gun.transform.forward * bulletSpeed;
 
-            Destroy(clone.gameObject, 3);
+            Destroy(clone.gameObject, bulletLifeTime);
         }
     }
 }
